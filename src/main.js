3d493f492dd722b4575d17679a1b16b24cd0d93f
LuigiM <jlpadilla@innovacion.gob.sv>
2025-05-22 14:08:53 -0600

    Feat: Configuracion de tailwind

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-
+import './style.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
@@ -10,7 +10,13 @@ const app = createApp(App)
 app.use(router)
 app.use(PrimeVue, {
     theme: {
-        preset: Aura
+        preset: Aura,
+        options: {
+            cssLayer: {
+                name: 'primevue',
+                order: 'theme, base, primevue'
+            }
+        }
     }
 })
 
--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import Components from 'unplugin-vue-components/vite';
 import { PrimeVueResolver } from '@primevue/auto-import-resolver';
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -15,7 +16,8 @@ export default defineConfig({
       resolvers: [
         PrimeVueResolver()
       ]
-    })
+    }),
+    tailwindcss()
   ],
   resolve: {
     alias: {
