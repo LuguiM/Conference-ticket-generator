da1e08f83071e59519a90b2c64659fa65dd6080d
LuigiM <jlpadilla@innovacion.gob.sv>
2025-05-27 09:08:48 -0600

    Fix: Base vite config

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Conference-ticket-generator/',
   plugins: [
     vue(),
     vueDevTools(),
